caeaebab5f960a88121ef691932c3abfc7ef95a0
EidmantasDev <eidmantas.slenys@gmail.com>
2024-04-04 21:04:36 +0300

    Added validation to StateLogin component onBlur

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -6,6 +6,13 @@ export default function Login() {
     password: '',
   });
 
+  const [didEdit, setDidEdit] = useState({
+    email: false,
+    password: false,
+  });
+
+  const emailIsInvalid = didEdit.email && !enteredValues.email.includes('@');
+
   function handleSubmit(event) {
     event.preventDefault();
 
@@ -16,6 +23,17 @@ export default function Login() {
     setEnteredValues((prevValues) => ({
       ...prevValues,
       [identifier]: value,
+    }));
+    setDidEdit((prevEdit) => ({
+      ...prevEdit,
+      [identifier]: false,
+    }));
+  }
+
+  function handleInputBlur(indentifier) {
+    setDidEdit((prevEdit) => ({
+      ...prevEdit,
+      [indentifier]: true,
     }));
   }
 
@@ -30,9 +48,13 @@ export default function Login() {
             id='email'
             type='email'
             name='email'
+            onBlur={() => handleInputBlur('email')}
             onChange={(event) => handleInputChange('email', event.target.value)}
             value={enteredValues.email}
           />
+          <div className='control-error'>
+            {emailIsInvalid && <p>Please enter a valid email address.</p>}
+          </div>
         </div>
 
         <div className='control no-margin'>
